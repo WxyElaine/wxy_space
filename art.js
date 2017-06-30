85074a9a8481b892f2fdfa650a44aad4cc9ecd8f
Xinyi Wang <wangx227@uw.edu>
2017-06-30 04:11:50 +0000

    Initial commit

--- art.js
+++ art.js
@@ -1,10 +1,17 @@
+"use strict";
+
+/*  
+    Xinyi Wang
+    06/29/17
+    Xinyi's Portfolio  
+    This file is the javascript for the web page "Xinyi's Portfolio".
+*/
+
 $(document).ready(function() {
-  
-$(".carousel").swiperight(function() {
-    $(this).carousel('prev');
-});
-$(".carousel").swipeleft(function() {  
-    $(this).carousel('next');
-});
- 
+    $(".carousel").swiperight(function() {
+        $(this).carousel('prev');
+    });
+    $(".carousel").swipeleft(function() {  
+        $(this).carousel('next');
+    });
 });
